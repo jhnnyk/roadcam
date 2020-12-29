f10e47bcc6b9fc1d02d3f0cb7bc717dfbeec867e
John Knotts <jhnnyk@gmail.com>
2020-12-29 16:49:14 -0700

    purge unused styles in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
   theme: {
     extend: {},
   },
