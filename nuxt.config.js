f76a70333452f877240dfd6d0b1eba8b065f4efb
John Knotts <jhnnyk@gmail.com>
2020-12-29 16:21:49 -0700

    title and description

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,15 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'roadcam',
+    title: 'road.camera',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'traffic cameras and road webcams from across the USA',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
