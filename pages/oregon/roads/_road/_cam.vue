19341dc835c540049bac760a6442919c6e19dc78
John Knotts <jhnnyk@gmail.com>
2021-01-02 16:32:07 -0700

    tweaks for SSG

--- pages/oregon/roads/_road/_cam.vue
+++ pages/oregon/roads/_road/_cam.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
-    {{ $route.params }}
-    <h1 class="text-4xl">{{ $route.params['device-name'] }}</h1>
-    <img :src="$route.params['cctv-url']" :alt="$route.params['device-name']" />
-    <p>{{ $route.params['cctv-other'] }}</p>
+    <h1 class="text-4xl">{{ cam['device-name'] }}</h1>
+    <img :src="cam['cctv-url']" :alt="cam['device-name']" />
+    <p>{{ cam['cctv-other'] }}</p>
   </div>
 </template>
+
+<script>
+import slugify from 'slugify'
+import tripCheckData from '../../roads/oregonroads.json'
+
+export default {
+  async asyncData({ params }) {
+    const cam = await tripCheckData.CCTVInventoryRequest.find(
+      (c) => slugify(c['device-name']) === params.cam
+    )
+    return { cam }
+  },
+}
+</script>
