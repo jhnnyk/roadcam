1809cb3755d9c7093236774763fc9aa435bb44be
John Knotts <jhnnyk@gmail.com>
2021-01-02 15:28:24 -0700

    individual cam pages

---
+++ pages/oregon/roads/_road/_cam.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    {{ $route.params }}
+    <h1 class="text-4xl">{{ $route.params['device-name'] }}</h1>
+    <img :src="$route.params['cctv-url']" :alt="$route.params['device-name']" />
+    <p>{{ $route.params['cctv-other'] }}</p>
+  </div>
+</template>
--- pages/oregon/roads/_road/index.vue
+++ pages/oregon/roads/_road/index.vue
@@ -3,26 +3,35 @@
     <h1 class="text-4xl">Oregon - {{ hwy }}</h1>
     <ul class="grid grid-cols-4">
       <li v-for="cam in cams" :key="cam.index">
-        {{ cam['device-name'] }}
-        <img :src="cam['cctv-url']" :alt="cam['device-name']" />
-        {{ cam['cctv-other'] }}
+        <nuxt-link :to="{ name: 'oregon-roads-road-cam', params: { ...cam } }">
+          {{ cam['device-name'] }}
+          <img :src="cam['cctv-url']" :alt="cam['device-name']" />
+          {{ cam['cctv-other'] }}
+        </nuxt-link>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import slugify from 'slugify'
 import tripCheckData from '../oregonroads.json'
 
 export default {
   async fetch() {
+    // filter cams for this hwy
     const filteredCams = await tripCheckData.CCTVInventoryRequest.filter(
       (cam) => {
         return cam['route-id'].toLowerCase() === this.$route.params.road
       }
     )
 
-    this.cams = filteredCams
+    // add slug for link
+    const camsWithSlug = await filteredCams.map((cam) => {
+      return { ...cam, cam: slugify(cam['device-name']) }
+    })
+
+    this.cams = camsWithSlug
   },
 
   data() {
