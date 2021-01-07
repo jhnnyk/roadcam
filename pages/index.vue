cf1421ed45e31343bde8d425dde19430a21cca7c
John Knotts <jhnnyk@gmail.com>
2021-01-07 11:52:15 -0700

    homepage content

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    <h2 class="text-4xl p-4">States</h2>
+    <h1 class="text-4xl pt-4">
+      Welcome to <nuxt-link :to="{ name: 'index' }">road.camera</nuxt-link>
+    </h1>
+    <p>Easily find webcams from all 50 states (coming soon)!</p>
+    <h4 class="text-xl p-4">Currently featuring over 2,000 webcams from:</h4>
     <ul>
       <li>
-        <nuxt-link to="/california">California</nuxt-link>
+        <h2 class="text-3xl">
+          <nuxt-link to="/california">California</nuxt-link>
+        </h2>
       </li>
       <li>
-        <nuxt-link to="/colorado">Colorado</nuxt-link>
+        <h2 class="text-3xl"><nuxt-link to="/colorado">Colorado</nuxt-link></h2>
       </li>
       <li>
-        <nuxt-link to="/oregon">Oregon</nuxt-link>
+        <h2 class="text-3xl"><nuxt-link to="/oregon">Oregon</nuxt-link></h2>
       </li>
     </ul>
   </div>
