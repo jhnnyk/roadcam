d2487b89e6a09910495c93b45ffe6a6b895d5eab
John Knotts <jhnnyk@gmail.com>
2021-01-09 18:26:45 -0700

    adshare ad code

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,26 @@
   <div class="text-gray-700 text-center">
     <TitleBar />
     <div class="container mb-8">
+      <div
+        class="_fa7cdd4c68507744"
+        data-zone="4b0fc271a7f04f37a3078af0f7f7489a"
+        style="
+          width: 320px;
+          height: 100px;
+          display: inline-block;
+          margin: 0 auto;
+        "
+      ></div>
+      <div
+        class="_fa7cdd4c68507744"
+        data-zone="9a903770e0e04138a43b210f1a96ce0e"
+        style="
+          width: 320px;
+          height: 100px;
+          display: inline-block;
+          margin: 0 auto;
+        "
+      ></div>
       <Nuxt />
     </div>
   </div>
